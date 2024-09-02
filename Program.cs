7ecf2247c440d5936509a9df4fb4431cd44b701e
yaelajami2 <y0556722091@gmail.com>
2024-09-02 18:22:23 +0300

    server air

--- Program.cs
+++ Program.cs
@@ -1,36 +1,18 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+ן»¿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebApplication1;
 
-namespace WebApplication1
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        CreateHostBuilder(args).Build().Run();
+    }
 
-
-public static IHostBuilder CreateHostBuilder(string[] args) =>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
-                Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>();
-
-                    });
-                
-// ודא ש-Startup מוגדרת כאן
             });
-
-    }
 }
--- Startup.cs
+++ Startup.cs
@@ -19,7 +19,10 @@ namespace WebApplication1
     {
         public Startup(IConfiguration configuration)
         {
+            
             Configuration = configuration;
+            
+
         }
 
         public IConfiguration Configuration { get; }
