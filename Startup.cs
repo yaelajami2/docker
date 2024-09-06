d3d8f7d226d35f23ed505bda1703be4c761b83d7
yaelajami2 <y0556722091@gmail.com>
2024-09-06 18:18:09 +0300

    swagger

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace api
 {
@@ -26,7 +20,6 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -50,15 +43,21 @@ namespace api
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
+            
+            // Use Swagger in all environments
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1");
+                c.RoutePrefix = string.Empty;  // Set Swagger UI at the app's root
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers(); // Use attribute routing
             });
         }
-
     }
 }
